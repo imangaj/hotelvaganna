10c82a0ae408bd68020dfa5bee9f4bdc6890304e
iman <iman.ganjalikhani@gmail.com>
2026-01-25 20:49:24 +0100

    Add migration for guest account profile

---
+++ backend/prisma/migrations/20260125194849_add_guest_account_profile/migration.sql
@@ -0,0 +1,4 @@
+-- AlterTable
+ALTER TABLE "guest_accounts" ADD COLUMN "firstName" TEXT;
+ALTER TABLE "guest_accounts" ADD COLUMN "lastName" TEXT;
+ALTER TABLE "guest_accounts" ADD COLUMN "phone" TEXT;
